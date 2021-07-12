f48d56605bed95f0723a2eb3607a341077c19aec
Andy-Lv <3304086606@qq.com>
2021-07-12 18:37:51 +0800

    day11

---
+++ day11/csv1.py
@@ -0,0 +1,19 @@
+"""
+读取CSV文件
+"""
+
+import csv
+
+filename = 'example.csv'
+
+try:
+    with open(filename)as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+except FileNotFoundError:
+    print('无法发开文件:', filename)
+else:
+    for item in data:
+        print('%-30s%-20s%-10s' % (item[0], item[1], item[2]))
+        
